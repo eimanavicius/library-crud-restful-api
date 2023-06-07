f7f857f525b03c40027ae3f0adf652e134dcf2d1
Nerijus Eimanavičius <nerijus.eimanavicius@telesoftas.com>
2023-06-07 19:52:06 +0300

    feat: redirect to homepage when accessing '/' path

--- src/http.ts
+++ src/http.ts
@@ -1,5 +1,10 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
+import { homepage } from '../package.json';
 
 const http: Express = express();
+
+http.get('/', (req: Request, res: Response): void => {
+    res.redirect(homepage);
+});
 
 export default http;
