dc84e833fff0a585db6e06f86ae3af4d6e05269e
Nerijus Eimanavičius <nerijus.eimanavicius@telesoftas.com>
2023-06-07 19:57:00 +0300

    feat: respond with Problem Detail to unhandled errors

--- src/http.ts
+++ src/http.ts
@@ -1,10 +1,16 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
 import { homepage } from '../package.json';
+import { ProblemDocument } from 'http-problem-details';
 
 const http: Express = express();
 
 http.get('/', (req: Request, res: Response): void => {
     res.redirect(homepage);
+});
+
+http.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+    console.error(err);
+    res.status(503).json(new ProblemDocument({status: 503}));
 });
 
 export default http;
